ad91ea4ed8405fc3ee31bfddb5a40eb305e39f08
Md Redowanul Islam <104225343+Silentt-Killer@users.noreply.github.com>
2022-05-21 19:03:54 +0600

    Create 282A - Bit++.cpp

---
+++ 282A - Bit++/282A - Bit++.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char first_value,midele_value,last_value;
+    int num,count=0;
+    scanf("%d",&num);
+    for (int i = 0; i < num; i++) {
+        // inside scanf first value use whitespech.
+        scanf(" %c%c%c",&first_value,&midele_value,&last_value);
+        if(midele_value =='+'){
+            count++;
+        } else if(midele_value=='-'){
+            count--;
+        }
+    }
+    printf("%d",count);
+    return 0;
+}
